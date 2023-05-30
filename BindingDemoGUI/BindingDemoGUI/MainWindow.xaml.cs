1c63409b4415ff69ecc3387c673a8cafe0e2e275
nguyyenminhvuuu <133886413+nguyyenminhvuuu@users.noreply.github.com>
2023-05-31 05:42:00 +0700

    update

---
+++ BindingDemoGUI/BindingDemoGUI/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BindingDemoGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+            private string btnNameBinding;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public string BtnNameBinding
+            {
+                get { return btnNameBinding; }
+                set { btnNameBinding = value; OnPropertyChanged(BtnNameBinding); }
+            }
+
+            public MainWindow()
+            {
+                InitializeComponent();
+               // this.DataContext = this;
+                BtnNameBinding = "Binding data nek";
+            }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }    
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1= new Window1(); 
+            window1.Show();
+        }
+    }
+}
